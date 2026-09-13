39f2f5bacd91fc4c4d0ae76a66cc6271c9292bcd
Matheo Cahitte <matheo.cahitte@student-cs.fr>
2026-09-13 17:34:10 +0200

    fixed url

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,6 @@
 (function () {
   // TODO: replace with your deployed backend URL, e.g. https://your-app.onrender.com/move
-  const BACKEND_URL = "https://abchess.onrender.com";
+  const BACKEND_URL = "https://abchess.onrender.com/move";
 
   const game = new Chess();
   let board = null;
